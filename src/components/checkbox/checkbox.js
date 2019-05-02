7bf83b6ddcc42279c39c867b0284f3f8fa6d614e
neyron163 <andrey163783@gmail.com>
2019-05-02 22:19:40 +0400

    checkbox arrow func

--- src/components/checkbox/checkbox.js
+++ src/components/checkbox/checkbox.js
@@ -3,31 +3,29 @@ import s from "./checkbox.module.css";
 import classNames from "classnames";
 import { ALL } from "../../data";
 
-export function Checkbox({ onClick, onlyCurrent, stop, data, checked }) {
-  return (
-    <div className={s.checkbox}>
-      <div onClick={() => onClick(stop)} className={s.choosen}>
-        <div
-          className={classNames(
-            { [s.check]: stop === ALL ? checked : data.includes(stop) },
-            s.handle
-          )}
-        />
-        {stop === ALL ? (
-          <div className={s.text}>Все пересадки</div>
-        ) : stop === 0 ? (
-          <div className={s.text}>Без пересадок</div>
-        ) : (
-          <div className={s.text}>
-            {stop === 1 ? `${stop} пересадка` : `${stop} пересадки`}
-          </div>
+export const Checkbox = ({ onClick, onlyCurrent, stop, data, checked }) => (
+  <div className={s.checkbox}>
+    <div onClick={() => onClick(stop)} className={s.choosen}>
+      <div
+        className={classNames(
+          { [s.check]: stop === ALL ? checked : data.includes(stop) },
+          s.handle
         )}
-      </div>
-      {onlyCurrent && (
-        <div className={s.only} onClick={() => onlyCurrent(stop)}>
-          только
+      />
+      {stop === ALL ? (
+        <div className={s.text}>Все пересадки</div>
+      ) : stop === 0 ? (
+        <div className={s.text}>Без пересадок</div>
+      ) : (
+        <div className={s.text}>
+          {stop === 1 ? `${stop} пересадка` : `${stop} пересадки`}
         </div>
       )}
     </div>
-  );
-}
+    {onlyCurrent && (
+      <div className={s.only} onClick={() => onlyCurrent(stop)}>
+        только
+      </div>
+    )}
+  </div>
+);
