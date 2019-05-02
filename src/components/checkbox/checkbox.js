f29da2cd3b37a94b9dc39e3c3ab633f37110c0d7
neyron163 <andrey163783@gmail.com>
2019-05-02 14:30:48 +0400

    fix mistake

--- src/components/checkbox/checkbox.js
+++ src/components/checkbox/checkbox.js
@@ -1,21 +1,21 @@
 import React from "react";
 import s from "./checkbox.module.css";
 import classNames from "classnames";
-import {ALL} from '../../data';
+import { ALL } from "../../data";
 
 export function Checkbox({ onClick, onlyCurrent, stop, checked }) {
   return (
     <div className={s.checkbox}>
       <div onClick={() => onClick(stop)} className={s.choosen}>
-        <div
-          className={classNames({ [s.check]: checked }, s.handle)}
-        />
+        <div className={classNames({ [s.check]: checked }, s.handle)} />
         {stop === ALL ? (
           <div className={s.text}>Все пересадки</div>
         ) : stop === 0 ? (
           <div className={s.text}>Без пересадок</div>
         ) : (
-          <div className={s.text}>{stop} пересадки</div>
+          <div className={s.text}>
+            {stop === 1 ? `${stop} пересадка` : `${stop} пересадки`}
+          </div>
         )}
       </div>
       {onlyCurrent && (
